d9a64b84ffcccf33fb52ba9903df0a8675d489b0
Michiel Vervaart <michiel.vervaart@xxllnc.nl>
2025-08-15 12:33:09 +0200

    Default login etc.

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -97,7 +97,7 @@
             </a>
 
             <flux:navlist variant="outline">
-                <flux:navlist.group :heading="__('Platform')">
+                <flux:navlist.group :heading="__('Main features')">
                     <flux:navlist.item icon="layout-grid" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
                     {{ __('Dashboard') }}
                     </flux:navlist.item>
@@ -107,10 +107,6 @@
             <flux:spacer />
 
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-                </flux:navlist.item>
-
                 <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
                 {{ __('Documentation') }}
                 </flux:navlist.item>
